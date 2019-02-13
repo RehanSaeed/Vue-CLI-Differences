1e7becc6e2a2af2f715847799046c279f2c34527
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2019-02-13 16:50:05 +0000

    3.3.0

--- Update.ps1
+++ Update.ps1
@@ -1,11 +1,13 @@
-$version = "3.2.0";
+$version = "3.3.0";
 
 npm install -g @vue/cli@$version
 Remove-Item .\Source -Force -Recurse;
 vue create --preset 'Rehans Preset' ummati
 Rename-Item -Path Ummati -NewName Source;
+git checkout master
 git add .
 git commit -m "$version"
 git push
 git branch $version
 git push
+git checkout master
