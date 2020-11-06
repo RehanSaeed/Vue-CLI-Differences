18b097f089978472463e1c28ef22ce62d18fa9f9
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-11-06 16:06:31 +0000

    Rename master branch to main

--- Update.ps1
+++ Update.ps1
@@ -4,10 +4,10 @@ npm install -g @vue/cli@$version
 Remove-Item .\Source -Force -Recurse;
 vue create --preset 'Rehans Preset' ummati
 Rename-Item -Path Ummati -NewName Source;
-git checkout master
+git checkout main
 git add .
 git commit -m "$version"
 git push
 git branch $version
 git push
-git checkout master
+git checkout main
